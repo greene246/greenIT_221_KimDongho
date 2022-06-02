c65de38e60beec864e5c8676d9b2e885fd42a06f
green <green@192.168.219.112>
2022-06-02 16:35:19 +0900

    완료

--- atm1/src/atm/Main.java
+++ atm1/src/atm/Main.java
@@ -3,20 +3,6 @@ package atm;
 public class Main {
 
 	public static void main(String[] args) {
-		// atm
-		// �� ȸ�� �� ��� ���¸� �ִ� 3������ ������ ������
-		
-		// 1. ȸ������ / Ż��
-		// �� User class
-		// �� UserManager class
-		// 2. ���°��� . ����
-		// �� Account class
-		// �� AccountManager class
-		
-		// 3. atm �
-		// �� Bank class (run())
-		
-		
 		Bank.instance.setName("Green");
 		Bank.instance.run();
 		
